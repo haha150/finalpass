a99fb532c464c8f2be6067ca803371d66c1b7f9a
haha150 <alisymeri@hotmail.com>
2023-08-17 22:32:24 +0200

    installer

--- installer/build2.sh
+++ installer/build2.sh
@@ -5,7 +5,7 @@ CGO_ENABLED=1 go build -C packages/se.symeri.finalpass/data/ -tags=release -ldfl
 rm -r packages/se.symeri.finalpass/data/*
 mv packages/se.symeri.finalpass/data/.finalpass.exe packages/se.symeri.finalpass/data/finalpass.exe
 cp ../desktop/config.env packages/se.symeri.finalpass/data/
-cp ../desktop/qtbox packages/se.symeri.finalpass/data/
+cp -r ../desktop/qtbox packages/se.symeri.finalpass/data/
 cp -r ../desktop/icons packages/se.symeri.finalpass/data/
 /c/Qt/QtIFW-4.6.0/bin/binarycreator -f -c config/config.xml -p packages/ -t /home/ubuntu/Qt/QtIFW-4.6.0/bin/installerbase finalpass-installer
 mv finalpass-installer dist/
