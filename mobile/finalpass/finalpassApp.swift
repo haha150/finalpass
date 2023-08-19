b280da8d1a09f086ccd77df70bdf5bd6dc3fc93f
haha150 <alisymeri@gmail.com>
2023-08-19 10:02:35 +0200

    init mobile

---
+++ mobile/finalpass/finalpassApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalpassApp.swift
+//  finalpass
+//
+//  Created by Ali Symeri on 2023-08-18.
+//
+
+import SwiftUI
+
+@main
+struct finalpassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
