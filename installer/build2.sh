86652457ea1c58a05ad5a3a0a07a8c3c60684663
haha150 <alisymeri@hotmail.com>
2023-09-05 21:07:09 +0200

    qt

--- installer/build2.sh
+++ installer/build2.sh
@@ -7,7 +7,7 @@ mv packages/se.symeri.finalpass/data/.finalpass.exe packages/se.symeri.finalpass
 cp ../desktop/config.env packages/se.symeri.finalpass/data/
 cp -r ../desktop/qtbox packages/se.symeri.finalpass/data/
 cp -r ../desktop/icons packages/se.symeri.finalpass/data/
-/c/Qt/QtIFW-4.6.0/bin/binarycreator.exe -f -c config/config.xml -p packages/ -t /c/Qt/QtIFW-4.6.0/bin/installerbase.exe finalpass-installer.exe
+/c/Qt/QtIFW-4.6.1/bin/binarycreator.exe -f -c config/config.xml -p packages/ -t /c/Qt/QtIFW-4.6.0/bin/installerbase.exe finalpass-installer.exe
 mkdir dist
 mv finalpass-installer.exe dist/
 rm -r packages/se.symeri.finalpass/data/*
